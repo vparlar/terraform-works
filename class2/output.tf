8ce6ebfede471cacb947b4a8938c1489b3cdb356
Vedat Parlar <vedat4evolve@gmail.com>
2020-04-28 08:48:12 +0000

    A

--- class2/output.tf
+++ class2/output.tf
@@ -1,16 +1,7 @@
-output "name" {
-    value = "vedat"
-}
-
-output "lastname" {
-    value = "parlar"
-}
-
-resource "aws_key_pair" "us-east-2-key" {
-  key_name = "digital_bashion2"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
-
-output "key_name" {
-    value = "aws_key_pair.us-east-2-key.key_name"
+resource "aws_instance" "web" {
+  ami             = "ami-0323c3dd2da7fb37d"
+  instance_type   = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
 }
