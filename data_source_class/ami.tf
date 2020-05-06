68baa4e2fc84ee80aafbff3962c0734c41010451
Vedat Parlar <vedat4evolve@gmail.com>
2020-05-06 08:06:51 +0000

    A

---
+++ data_source_class/ami.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "2.59"
+}
+data "aws_ami" "centos" {
+  most_recent = true
+  owners      = ["679593333241"]
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+}
+resource "aws_key_pair" "us-east-1-key" {
+  key_name   = "farrukhs_bastion"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+resource "aws_instance" "centos" {
+  ami           = "${data.aws_ami.centos.id}"
+  instance_type = "t2.micro"
+  key_name      =   "${aws_key_pair.us-east-1-key.key_name}"
+  provisioner "remote-exec" {
+    connection {
+        type            = "ssh"
+        user            = "centos"
+        private_key     = "${file("~/.ssh/id_rsa")}"
+        host            = "${self.public_ip}"
+    }
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo systemctl start httpd ",
+    ]
+  }
+}
